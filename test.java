545926c0f03ee9766f5574a9ccc701d5ede46594
DwongBeTeo <duongdangtran54321@gmail.com>
2025-10-22 20:40:42 +0700

    Day06-responsive

---
+++ test.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class test {
+    ///tính trung bình 3 số:
+    // public static void main(String[] args) {
+    //     int result = average(5, 10, 15);
+    //     System.out.println("Average: " + result);
+    // }
+    // public static int average(int a,int b,int c){
+    //     return (a+b+c)/3;
+    // }
+
+    /// kiểm tra chẵn lẻ:
+    // public static void main(String[] args) {
+    //     String result = checked(11);
+    //     System.out.println("Check: " + result);
+    // }
+    // public static String checked(int input) {
+    //     if(input%2==0){
+    //         return "Even"; //chẵn
+    //     }else{
+    //         return "Odd"; //lẻ
+    //     }
+    // }
+
+    ///in bảng cửu chương
+    // public static void main(String[] args) {
+    //     List<Integer> table = multiplicationTable(5);
+    //     System.out.println("Multiplication Table: " + table);
+    // }
+    // public static List<Integer> multiplicationTable(int n) {
+    //     List<Integer> table = new ArrayList<>();
+    //     for (int i = 1; i <= 10; i++) {
+    //         table.add(n * i);
+    //     }
+    //     return table;
+    // }
+
+    public static void main(String[] args) {
+        // int[][] matrix = new int[2][3];
+        // int[][] matrix2 = {{1, 2, 3}, {4, 5, 6}};
+        // int x = matrix2[1][2];
+        // System.out.println("Element at row 2, column 3: " + x);
+        // for(int i=0; i<matrix2.length; i++){
+        //     for(int j=0; j<matrix2[i].length; j++){
+        //         System.out.print(matrix2[i][j] + " ");
+        //     }
+        //     // System.out.println();
+        // }
+        double a = 200;
+        Double b = 200.0;
+        System.out.println(a == b);
+    }
+}
