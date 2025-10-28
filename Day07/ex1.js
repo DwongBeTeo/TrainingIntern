654ff48816d69c9bc296a92e58d89a2f39cdd25e
DwongBeTeo <duongdangtran54321@gmail.com>
2025-10-28 22:54:02 +0700

    day08

---
+++ Day07/ex1.js
@@ -0,0 +1,9 @@
+var yourName = "Tran dang duong";
+console.log("welcome ", yourName);
+var age = 21;
+console.log("welcome ", yourName , "your age is ", age);
+
+var lastName = "Tran";
+let thisAge = 22;
+const pi = 3.14;
+console.log(lastName, thisAge, pi);
